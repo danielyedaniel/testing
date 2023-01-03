f18864fa1f5aba32846db88a6bd7f1d766cfdb37
Daniel <yedaniel99@gmail.com>
2023-01-03 13:34:38 -0500

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Daniel Ye on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
