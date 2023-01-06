7b1fed279582538390b3f7549b2dc11e7fbd53f3
Daniel <yedaniel99@gmail.com>
2023-01-05 21:50:15 -0500

    api works

--- testing/ContentView.swift
+++ testing/ContentView.swift
@@ -7,17 +7,44 @@
 
 import SwiftUI
 
+import Foundation
+
 struct ContentView: View {
+    @State private var response: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Response: \(response)")
+            Button(action: sendPostRequest) {
+                Text("Send POST request")
+            }
         }
-        .padding()
+    }
+
+    func sendPostRequest() {
+        let url = URL(string: "https://www.wangevan.com/user/login")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let postData = [
+            "email": "testuser@gmail.com",
+            "password": "testpassword"
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: postData)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                self.response = "Error: \(error)"
+                return
+            }
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                self.response = dataString
+            }
+        }.resume()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
